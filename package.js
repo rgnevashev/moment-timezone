e02d03396bd531fd2e4ecde82247bb635593a5a6
roman <rgnevashev@gmail.com>
2016-03-30 22:13:08 +0300

    meteor package

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'rgnevashev:moment-timezone',
+  version: '0.5.0',
+  summary: 'Parse and display moments in any timezone.',
+  git: 'https://github.com/moment/moment-timezone.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  //api.versionsFrom('1.2.1');
+  api.use('momentjs:moment@2.11.1');
+  api.imply('momentjs:moment');
+  api.addFiles([
+    'require-shim.js',
+    'builds/moment-timezone-with-data.js',
+    'revert-require-shim.js'
+  ], ['client', 'server']);
+});
